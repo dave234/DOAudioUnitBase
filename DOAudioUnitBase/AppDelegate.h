0848540442256ff0fb5aae5b55cbc28068c74d08
dave <dave@daves-MacBook-Pro.local>
2017-09-13 13:48:11 -0700

    Created iOS project

---
+++ DOAudioUnitBase/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DOAudioUnitBase
+//
+//  Created by dave on 9/13/17.
+//  Copyright © 2017 oneill. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DOAudioUnitBase/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DOAudioUnitBase
+//
+//  Created by dave on 9/13/17.
+//  Copyright © 2017 oneill. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
